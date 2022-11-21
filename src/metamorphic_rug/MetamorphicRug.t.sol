91539c5c403199e5ccdc77a232eb092bb1fb1005
spalen0 <spalen@proton.me>
2022-11-22 00:33:37 +0100

    feat: add metamorphic example

--- src/metamorphic_rug/MetamorphicRug.t.sol
+++ src/metamorphic_rug/MetamorphicRug.t.sol
@@ -14,17 +14,49 @@ contract MetamorphicRug is Test {
 
     function setUp() public {
         grandparentContract = new Grandparent();
-		parentAddr =  grandparentContract.deployParent(1);
+        bytes memory bytecode = abi.encodePacked(vm.getCode("Parent.sol"));
+		parentAddr =  grandparentContract.deploy(1, bytecode);
         childAddr = Parent(parentAddr).deployChild();
-		
-		assertTrue(parentAddr == address(grandparentContract.parent()));
+
 		// The selfdestruct call must be done in setUp() due to a foundry limitation: https://github.com/foundry-rs/foundry/issues/1543
 		Parent(parentAddr).delegateFunction();
 		Child(childAddr).destroy();
+
+		// Never make assertions in the setUp function. Failed assertions won't result with failed test.
+		// https://book.getfoundry.sh/tutorials/best-practices?highlight=setup#general-test-guidance
 	}
 
-	function testMetamorphic() public {
-		assertTrue(parentAddr == address(grandparentContract.parent()));
+	// deploying the same contract Parent with the same salt will produce the same address
+	function testTheSameContract() public {
+        bytes memory bytecode = abi.encodePacked(vm.getCode("Parent.sol"));
+		address newParrentAddr =  grandparentContract.deploy(1, bytecode);
+		assertEq(newParrentAddr, parentAddr);
+
+		uint256 add = 5;
+		assertEq(Parent(parentAddr).balance(), 0);
+		Parent(parentAddr).addBalance(add);
+		assertEq(Parent(parentAddr).balance(), add);
 	}
 
+	// deploying the same contract Parent but with a different salt will produce a different address
+	function testDifferentSalt() public {
+        bytes memory bytecode = abi.encodePacked(vm.getCode("Parent.sol"));
+		address newParrentAddr = grandparentContract.deploy(2, bytecode);
+		// different salt will produce different parent address
+		assertTrue(parentAddr != newParrentAddr);
+	}
+
+	// deploying a different contract Parent2 with the same salt will produce the same address
+	// new contract will be on the same address but with a different implementation potentially malicious
+	function testDifferentContract() public {
+        bytes memory bytecode = abi.encodePacked(vm.getCode("Parent2.sol"));
+		address newParrentAddr =  grandparentContract.deploy(1, bytecode);
+		assertEq(newParrentAddr, parentAddr);
+
+		uint256 add = 5;
+		assertEq(Parent(parentAddr).balance(), 0);
+		Parent(parentAddr).addBalance(add);
+		// Parent2 will double add balance
+		assertEq(Parent(parentAddr).balance(), add * 2);
+	}
 }
--- src/metamorphic_rug/Parent.sol
+++ src/metamorphic_rug/Parent.sol
@@ -5,6 +5,7 @@ import {Child} from "./Child.sol";
 
 contract Parent {
     Child public _contract;
+	uint256 public balance;
 
     function deployChild() external returns (address) {
         _contract = new Child();
@@ -14,9 +15,13 @@ contract Parent {
     function delegateFunction() public {
         // This delegatecall will destroy this Parent.sol contract
 		// After Parent.sol is destroyed, destroy() in Child.sol can be called directly
-		// Then a new Parent.sol and a new Child.sol can be deployed
+		// Then a new Parent.sol or a different Parent2.sol can be deployed
 		bool status;
 		(status, ) = address(_contract).delegatecall(abi.encodeWithSignature("destroy()"));
 		if (!status) revert();
+	}
+
+	function addBalance(uint256 _add) external returns (uint256) {
+		balance += _add;
 	}
 }
